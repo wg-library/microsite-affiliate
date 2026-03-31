e05598f28cb372d6861ea1f744f9e861ba4483c6
Dito <56707407+wg-library@users.noreply.github.com>
2026-03-31 13:34:25 +0700

    Update loader-widget.js

--- loader-widget.js
+++ loader-widget.js
@@ -5,19 +5,21 @@
   const baseUrl = `https://cdn.jsdelivr.net/gh/${user}/${repo}@${branch}/`;
   const version = new Date().getTime();
 
+  const cssUrl = `${baseUrl}wg-microsite-affiliate.css?v=${version}`;
+
   const pre = document.createElement('link');
   pre.rel = 'preload';
   pre.as = 'style';
-  pre.href = `${baseUrl}wg-microsite-affiliate.min.css?v=${version}`;
+  pre.href = cssUrl;
   document.head.appendChild(pre);
 
   const link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = `${baseUrl}wg-microsite-affiliate.min.css?v=${version}`;
+  link.href = cssUrl;
   document.head.appendChild(link);
 
   const script = document.createElement('script');
-  script.src = `${baseUrl}vcore-free.min.js?v=${version}`;
+  script.src = `${baseUrl}vcore-free.js?v=${version}`;
   script.defer = true;
   document.body.appendChild(script);
 })();
